6c62fe912515f4c33f19f05e2f9acc3112110bd5
Tom Nunn <tom@emovia.net>
2023-04-16 20:49:46 +0100

    Make request fire only on reaching requestMinInputLength if requestDebounceDelay set to 0

--- src/Select.elm
+++ src/Select.elm
@@ -293,9 +293,21 @@ request effect =
     UpdateOptions.Request effect
 
 
-{-| Configure debouncing for the request. How long should we wait in milliseconds after the user stops typing to send the request? Default is 300.
+{-| Configure debouncing for the request.
+How long should we wait in milliseconds after the user stops typing to send the request? Default is 300.
 
-    Select.updateWith [ Select.request fetchThings, Select.requestDebounceDelay 500 ] SelectMsg subMsg model.select
+If set to 0, a request will be immediately whenever the input length reaches requestMinInputLength,
+no further requests will be sent on typing beyond that length and beyond that filtering will be done client side.
+This can make the input feel more responsive (options appear sooner),
+but may result in a larger result payload requiring more filtering on the client side.
+
+    Select.updateWith
+        [ Select.request fetchThings
+        , Select.requestDebounceDelay 200
+        ]
+        SelectMsg
+        subMsg
+        model.select
 
 -}
 requestDebounceDelay : Float -> UpdateOption err a msg
