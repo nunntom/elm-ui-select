3ca0f6e5bc70f62c084a25b5d2fd5741371693e7
Tom Nunn <tom@emovia.net>
2023-04-21 21:22:10 +0100

    Add switch script

---
+++ switch.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+if [ -z "$(git status --porcelain)" ]; then 
+  # Working directory clean
+    if [ $1 == 'elm-ui' ]
+    then
+        echo "switching to elm-ui"
+        mv src/Select/ElmUi.elm src/Select.elm
+        rm src/Select/ElmCss.elm
+        rm src/Internal/View/ElmCss.elm
+        rm -rfd examples/src/ElmCss
+        rm -rfd examples/tests/ElmCss
+        find . -type f -name "*.elm" -exec sed -i 's/import Select\.ElmUi/import Select/g' {} +
+        find . -type f -name "*.elm" -exec sed -i 's/module Select\.ElmUi/module Select/g' {} +
+        sed -i ':a;N;$!ba;s/\s\+"Select\.ElmCss",//g' elm.json
+        sed -i 's/"Select\.ElmUi",/Select/g' elm.json
+        npx elm-json uninstall rtfeldman/elm-css --yes
+    elif [ $1 == 'elm-css' ]
+        echo "switching to elm-css"
+        mv src/Select/ElmCss.elm src/Select.elm
+        rm src/Select/ElmUi.elm
+        rm src/Internal/View/ElmUi.elm
+        rm -rfd examples/src/ElmUi
+        rm -rfd examples/tests/ElmUi
+        find . -type f -name "*.elm" -exec sed -i 's/import Select\.ElmCss/import Select/g' {} +
+        find . -type f -name "*.elm" -exec sed -i 's/module Select\.ElmCss/module Select/g' {} +
+        sed -i ':a;N;$!ba;s/\s\+"Select\.ElmUi",//g' elm.json
+        sed -i 's/"Select\.ElmCss",/Select/g' elm.json
+        npx elm-json uninstall mdgriffith/elm-ui --yes
+    then
+        echo "css"
+    else 
+        echo "No valid variant specified"
+    fi
+else 
+  echo "There are uncommitted changes!"
+fi 
