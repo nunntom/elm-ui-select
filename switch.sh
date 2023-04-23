531ec422702418f343fa83b7303513e4347ceb30
Tom Nunn <tom@emovia.net>
2023-04-23 20:19:48 +0100

    Switch readmes

--- switch.sh
+++ switch.sh
@@ -10,11 +10,14 @@ if [ -z "$(git status --porcelain)" ]; then
         rm src/Internal/View/ElmCss.elm
         rm -rfd examples/src/ElmCss
         rm -rfd examples/tests/ElmCss
+        rm README.md
+        mv README.elm-ui.md README.md
         find . -type f -name "*.elm" -exec sed -i 's/import Select\.ElmUi as Select/import Select/g' {} +
         find . -type f -name "*.elm" -exec sed -i 's/module Select\.ElmUi/module Select/g' {} +
         sed -i ':a;N;$!ba;s/\s\+"Select\.ElmCss",//g' elm.json
         sed -i 's/"Select\.ElmUi",/"Select",/g' elm.json
         npx elm-json uninstall rtfeldman/elm-css --yes
+        cd examples && npx elm-json uninstall rtfeldman/elm-css --yes
     elif [ $1 == 'elm-css' ]
         echo "switching to elm-css"
         mv src/Select/ElmCss.elm src/Select.elm
@@ -22,11 +25,14 @@ if [ -z "$(git status --porcelain)" ]; then
         rm src/Internal/View/ElmUi.elm
         rm -rfd examples/src/ElmUi
         rm -rfd examples/tests/ElmUi
+        rm README.md
+        mv README.elm-css.md README.md
         find . -type f -name "*.elm" -exec sed -i 's/import Select as Select\.ElmCss/import Select/g' {} +
         find . -type f -name "*.elm" -exec sed -i 's/module Select\.ElmCss/module Select/g' {} +
         sed -i ':a;N;$!ba;s/\s\+"Select\.ElmUi",//g' elm.json
         sed -i 's/"Select\.ElmCss",/"Select",/g' elm.json
         npx elm-json uninstall mdgriffith/elm-ui --yes
+        cd examples && npx elm-json uninstall mdgriffith/elm-ui --yes
     then
         echo "css"
     else 
